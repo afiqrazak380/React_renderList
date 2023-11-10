03269621b9a74170bf7a947e6721c6f9d23a755f
AfiqRazak <afiqrazak380@gmail.com>
2023-11-10 20:20:28 +0000

    Add filter method

--- renderList/src/List.jsx
+++ renderList/src/List.jsx
@@ -1,19 +1,24 @@
 
 function List(){
 
-  const fruits = [{name: "apple", calories: 95},
-                  {name: "orange", calories: 45},
-                  {name: "banana", calories: 105},
-                  {name: "coconut", calories: 159},
-                  {name: "pineapple", calories: 37}];
+  const fruits = [{id: 1, name: "apple", calories: 95},
+                  {id: 2, name: "orange", calories: 45},
+                  {id: 3, name: "banana", calories: 105},
+                  {id: 4, name: "coconut", calories: 159},
+                  {id: 5, name: "pineapple", calories: 37}];
 
   // fruits.sort((a, b) => a.name.localeCompare(b.name)) // alphabetical
   // fruits.sort((a, b) => b.name.localeCompare(a.name)) // reverse alphabetical
   // fruits.sort((a,b) => a.calories - b.calories) // numeric
-  fruits.sort((a,b) => b.calories - a.calories) // reverse numeric
+  // fruits.sort((a,b) => b.calories - a.calories) // reverse numeric
+
+  // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100) // low calories fruits
+  // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100) // high calories fruits
                   
-  const listItems= fruits.map(fruit => <li key={fruit.name}>
-                                           {fruit.name}: &nbsp; <b>{fruit.calories}</b></li>)
+  const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>
+                                                    {highCalFruit.name}: &nbsp; 
+                                                    <b>{highCalFruit.calories}</b>
+                                                    </li>)
 
   return(<ol>{listItems}</ol>)
 }
