f59fccc79f9edbf8a3b0f54a36314132a14abe17
AfiqRazak <afiqrazak380@gmail.com>
2023-11-10 20:49:32 +0000

    Add reuseable list component

--- renderList/src/App.jsx
+++ renderList/src/App.jsx
@@ -2,10 +2,24 @@
 import List from "./List.jsx"
 
 function App() {
+
+  const fruits = [{id: 1, name: "apple", calories: 95},
+                  {id: 2, name: "orange", calories: 45},
+                  {id: 3, name: "banana", calories: 105},
+                  {id: 4, name: "coconut", calories: 159},
+                  {id: 5, name: "pineapple", calories: 37}];
+
+  const vegetables = [{id: 6, name: "potato", calories: 90},
+                      {id: 7, name: "carrots", calories: 46},
+                      {id: 8, name: "celery", calories: 23},
+                      {id: 9, name: "corn", calories: 19},
+                      {id: 10, name: "brocoli", calories: 38}];  
+        
   
   return (
     <>
-     <List/> 
+     <List items = {fruits} category="Fruits"/>
+     <List items = {vegetables} category = "Vegetables"/>
     </>
   )
 }
--- renderList/src/List.jsx
+++ renderList/src/List.jsx
@@ -1,26 +1,29 @@
 
-function List(){
+function List(props){
 
-  const fruits = [{id: 1, name: "apple", calories: 95},
-                  {id: 2, name: "orange", calories: 45},
-                  {id: 3, name: "banana", calories: 105},
-                  {id: 4, name: "coconut", calories: 159},
-                  {id: 5, name: "pineapple", calories: 37}];
+  const category = props.category
 
-  // fruits.sort((a, b) => a.name.localeCompare(b.name)) // alphabetical
+  const itemList = props.items
+
+  const listItems = itemList.map(item => <li key={item.id}>
+                                          {item.name}: &nbsp; 
+                                        <b>{item.calories}</b>
+                                        </li>);
+
+  return(<>
+            <h3>{category}</h3>
+            <ol>{listItems}</ol>
+          </>);
+}
+ 
+export default List
+
+
+
+// fruits.sort((a, b) => a.name.localeCompare(b.name)) // alphabetical
   // fruits.sort((a, b) => b.name.localeCompare(a.name)) // reverse alphabetical
   // fruits.sort((a,b) => a.calories - b.calories) // numeric
   // fruits.sort((a,b) => b.calories - a.calories) // reverse numeric
 
   // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100) // low calories fruits
   // const highCalFruits = fruits.filter(fruit => fruit.calories >= 100) // high calories fruits
-                  
-  const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>
-                                                    {highCalFruit.name}: &nbsp; 
-                                                    <b>{highCalFruit.calories}</b>
-                                                    </li>)
-
-  return(<ol>{listItems}</ol>)
-}
-
-export default List
