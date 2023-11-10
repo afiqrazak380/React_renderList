51464ebadb97e502ec4f43230672149fcc234f2f
AfiqRazak <afiqrazak380@gmail.com>
2023-11-10 17:57:54 +0000

    Adding key ID

--- renderList/src/List.jsx
+++ renderList/src/List.jsx
@@ -1,9 +1,13 @@
 
 function List(){
 
-  const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+  const fruits = [{name: "apple", calories: 95},
+                  {name: "orange", calories: 45},
+                  {name: "banana", calories: 105},
+                  {name: "coconut", calories: 159},
+                  {name: "pineapple", calories: 37}];
 
-  const listItems= fruits.map(fruit => <li>{ fruit}</li>)
+  const listItems= fruits.map(fruit => <li key={fruit.name}>{fruit.name}</li>)
 
   return(<ol>{listItems}</ol>)
 }
